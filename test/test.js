ba0fd9e299bd8c86227ae89c79d48e9588f661d2
liao <mail@liaozhibin.com>
2019-08-18 18:17:17 +0800

    增加 smms

--- test/test.js
+++ test/test.js
@@ -3,23 +3,13 @@ const path = require('path');
 
 const testConfig = {
   server: 'smms',
-  secretKey: '1w8bK6Pgd9E5gq1DzaHzTiB2ErFUVVTd'
 }
 
-const testQiniuConfig = {
-  "server": "qiniu",
-  "accessKey": "jRqyPYM5eiubPWfzxVsPE9PsnV4N8MtgDOa-aTj_",
-  "secretKey": "aGsZsqdeWVLOFWx4WQvkjqPydSplMcnMglQgAX0l",
-  "bucket": "bigliao",
-  "baseURL": "https://cdn.bigliao.com/"
-};
 const testPic = path.resolve(__dirname, './pic.jpeg');
 
-describe('Test smms', () => {
-  it('upload', async () => {
-    return uper.upload(testPic, testConfig).then(res => {
-      // console.log(res);
-    });
-    
+describe('Test upload', function () {
+  it('upload to smms', async function () {
+    this.timeout(5000);
+    return uper.upload(testPic, testConfig);
   })
 });
